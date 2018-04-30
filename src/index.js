ec826f696b61e9ae9b38ef94c19bb3e1a23c7098
Kouty <kouty79@gmail.com>
2018-04-30 13:46:08 +0200

    Adds toJSON and fromString functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 (function (exports) {
   'use strict';
 
+  var parseRegex = /^LikeASymbol\((.*)\)$/;
+
   function LikeASymbol(_description) {
     var description = _description === undefined ? '' : _description;
 
     return {
       toString: function () {
         return 'LikeASymbol(' + description + ')';
-      }
+      },
+      toJSON() {
+        return this.toString();
+      },
+
     };
   }
+
+  LikeASymbol.fromString = function fromJSON(jsonValue) {
+    var match = jsonValue.match(parseRegex);
+    return new LikeASymbol(match[1]);
+  };
 
   exports.LikeASymbol = LikeASymbol;
 })(module && module.exports || self);
--- test/like-a-symbol.test.js
+++ test/like-a-symbol.test.js
@@ -6,7 +6,7 @@ describe('LikeASymbol', () => {
     expect(LikeASymbol()).not.to.equal(LikeASymbol());
   });
 
-  describe('toString', ()=> {
+  describe('toString', () => {
     it('should return "LikeASymbol()"', () => {
       expect(LikeASymbol().toString()).to.equal('LikeASymbol()');
     });
@@ -14,7 +14,31 @@ describe('LikeASymbol', () => {
     it('should return "LikeASymbol(description)" when using LikeASymbol("description")', () => {
       expect(LikeASymbol('description').toString()).to.equal('LikeASymbol(description)');
     });
+
+    it('should return "LikeASymbol(null)" when using LikeASymbol(null)', () => {
+      expect(LikeASymbol(null).toString()).to.equal('LikeASymbol(null)');
+    });
+
+    it('should return "LikeASymbol()" when using LikeASymbol(undefined)', () => {
+      expect(LikeASymbol(undefined).toString()).to.equal('LikeASymbol()');
+    });
   });
 
+  describe('JSON', () => {
+    it('should serialize with JSON.stringify() to "LikeAString(description)"', () => {
+      console.log(JSON.stringify({ a: LikeASymbol() }));
+      expect(JSON.stringify(LikeASymbol('description'))).to.equal('"LikeASymbol(description)"')
+    });
+
+    it('should provide a convenient static function fromString', () => {
+      const symbol = LikeASymbol('(a)');
+      expect(LikeASymbol.fromString(symbol.toJSON()).toString()).to.equal(symbol.toString());
+    });
+
+    xit('should consider "LikeASymbol(description)" parsable', () => {
+      expect(LikeASymbol.isParsable('LikeASymbol(description)')).to.equal(true);
+    });
+
+  });
 
 });
